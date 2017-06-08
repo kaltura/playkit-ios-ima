d7498d546e995154053dd3e48d6659199f1024aa
Eliza Sapir <eliza.sapir@kaltura.com>
2017-06-08 15:17:31 +0300

    replace file's header text

--- IMAPluginError.swift
+++ IMAPluginError.swift
@@ -1,16 +1,11 @@
 // ===================================================================================================
-//                           _  __     _ _
-//                          | |/ /__ _| | |_ _  _ _ _ __ _
-//                          | ' </ _` | |  _| || | '_/ _` |
-//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
-//
 // Copyright (C) 2017 Kaltura Inc.
 //
 // Licensed under the AGPLv3 license,
 // unless a different license for a particular library is specified in the applicable library path.
 //
 // You may obtain a copy of the License at
-// http://www.gnu.org/licenses/agpl-3.0.html
+// https://www.gnu.org/licenses/agpl-3.0.html
 // ===================================================================================================
 
 import Foundation
