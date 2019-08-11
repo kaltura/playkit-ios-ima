7da71149ba1acaf2da3d7ca22507f93303807112
Nilit Danan <Nilit.danan@kaltura.com>
2019-08-11 07:59:56 +0300

    FEM-2609 Added maxRedirects to IMAConfig. (#45)
    Code reordering.

--- Sources/IMAConfig.swift
+++ Sources/IMAConfig.swift
@@ -13,33 +13,35 @@ import GoogleInteractiveMediaAds
 
 @objc public class IMAConfig: NSObject {
     
+    // IMAAdsRequest
+    @objc public var adTagUrl: String = ""
+    @objc public var adsResponse: String = ""
+    @objc public var vastLoadTimeout: NSNumber?
+    
+    // IMASettings
+    @objc public var ppid: String?
+    @objc public var language: String = "en"
+    @objc public var maxRedirects: UInt = 4
     @objc public let enableBackgroundPlayback = true
     // Defaults to false, because otherwise ad breaks events will not happen.
     // We need to have control on whether ad break will start playing or not, using `Loaded` event is not enough.
     // (Will also need more safety checks for loaded because loaded will happen more than once).
     @objc public let autoPlayAdBreaks = false
-    @objc public var language: String = "en"
-
-    @objc public var videoBitrate = kIMAAutodetectBitrate
-    @objc public var videoMimeTypes: [String]?
-    
-    @objc public var adTagUrl: String = ""
-    @objc public var adsResponse: String = ""
-    
-    @objc public var ppid: String?
-    @objc public var companionView: UIView?
-    @objc public var webOpenerPresentingController: UIViewController?
-    /// Ads request timeout interval, when ads request will take more then this time, will resume content.
-    @objc public var requestTimeoutInterval: TimeInterval = IMAPlugin.defaultTimeoutInterval
+    @objc public var playerType: String = "kaltura-vp-ios"
+    @objc public var playerVersion: String?
     /// Enables debug mode on IMA SDK which will output detailed log information to the console.
     /// The default value is false.
     @objc public var enableDebugMode: Bool = false
     
-    @objc public var playerType: String = "kaltura-vp-ios"
-    @objc public var playerVersion: String?
+    // IMAAdsRenderingSettings
+    @objc public var videoMimeTypes: [String]?
+    @objc public var videoBitrate = kIMAAutodetectBitrate
+    @objc public var webOpenerPresentingController: UIViewController?
     
-    @objc public var vastLoadTimeout: NSNumber?
-    
+    // Extra Data
+    /// Ads request timeout interval, when ads request will take more then this time, will resume content.
+    @objc public var requestTimeoutInterval: TimeInterval = IMAPlugin.defaultTimeoutInterval
+    @objc public var companionView: UIView?
     @objc public var videoControlsOverlays: [UIView]?
     
     /// This boolean indicates whether or not to play the pre-roll when the start position is bigger then 0.
@@ -47,24 +49,6 @@ import GoogleInteractiveMediaAds
     @objc public var alwaysStartWithPreroll: Bool = false
     
     // Builders
-    @discardableResult
-    @nonobjc public func set(language: String) -> Self {
-        self.language = language
-        return self
-    }
-    
-    @discardableResult
-    @nonobjc public func set(videoBitrate: Int) -> Self {
-        self.videoBitrate = videoBitrate
-        return self
-    }
-    
-    @discardableResult
-    @nonobjc public func set(videoMimeTypes: [String]) -> Self {
-        self.videoMimeTypes = videoMimeTypes
-        return self
-    }
-    
     @discardableResult
     @nonobjc public func set(adTagUrl: String) -> Self {
         self.adTagUrl = adTagUrl
@@ -78,20 +62,20 @@ import GoogleInteractiveMediaAds
     }
     
     @discardableResult
-    @nonobjc public func set(companionView: UIView) -> Self {
-        self.companionView = companionView
+    @nonobjc public func set(vastLoadTimeout: NSNumber) -> Self {
+        self.vastLoadTimeout = vastLoadTimeout
         return self
     }
     
     @discardableResult
-    @nonobjc public func set(webOpenerPresentingController: UIViewController) -> Self {
-        self.webOpenerPresentingController = webOpenerPresentingController
+    @nonobjc public func set(language: String) -> Self {
+        self.language = language
         return self
     }
     
     @discardableResult
-    @nonobjc public func set(requestTimeoutInterval: TimeInterval) -> Self {
-        self.requestTimeoutInterval = requestTimeoutInterval
+    @nonobjc public func set(maxRedirects: UInt) -> Self {
+        self.maxRedirects = maxRedirects
         return self
     }
     
@@ -108,8 +92,32 @@ import GoogleInteractiveMediaAds
     }
     
     @discardableResult
-    @nonobjc public func set(vastLoadTimeout: NSNumber) -> Self {
-        self.vastLoadTimeout = vastLoadTimeout
+    @nonobjc public func set(videoMimeTypes: [String]) -> Self {
+        self.videoMimeTypes = videoMimeTypes
+        return self
+    }
+    
+    @discardableResult
+    @nonobjc public func set(videoBitrate: Int) -> Self {
+        self.videoBitrate = videoBitrate
+        return self
+    }
+    
+    @discardableResult
+    @nonobjc public func set(webOpenerPresentingController: UIViewController) -> Self {
+        self.webOpenerPresentingController = webOpenerPresentingController
+        return self
+    }
+    
+    @discardableResult
+    @nonobjc public func set(requestTimeoutInterval: TimeInterval) -> Self {
+        self.requestTimeoutInterval = requestTimeoutInterval
+        return self
+    }
+    
+    @discardableResult
+    @nonobjc public func set(companionView: UIView) -> Self {
+        self.companionView = companionView
         return self
     }
     
