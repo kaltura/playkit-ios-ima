617594b26591267eeaf3fa5caa56ec00ee67369d
Sergey Chausov <chouzoff@gmail.com>
2022-11-27 01:09:19 +0200

    FEC-12761 Code fixes according to IMA SDK API changes

--- Sources/iOS/IMADAIPlugin+AdDisplayContainer.swift
+++ Sources/iOS/IMADAIPlugin+AdDisplayContainer.swift
@@ -14,10 +14,16 @@ extension IMADAIPlugin {
     static func createAdDisplayContainer(forView view: UIView, viewController: UIViewController?, withCompanionView companionView: UIView? = nil) -> IMAAdDisplayContainer {
         // Setup ad display container and companion if exists, needs to create a new ad container for each request.
         if let cv = companionView {
-            let companionAdSlot = IMACompanionAdSlot(view: companionView, width: Int(cv.frame.size.width), height: Int(cv.frame.size.height))
-            return IMAAdDisplayContainer(adContainer: view, viewController: viewController, companionSlots: [companionAdSlot!])
+            let companionAdSlot = IMACompanionAdSlot(view: cv,
+                                                     width: Int(cv.frame.size.width),
+                                                     height: Int(cv.frame.size.height))
+            return IMAAdDisplayContainer(adContainer: view,
+                                         viewController: viewController,
+                                         companionSlots: [companionAdSlot])
         } else {
-            return IMAAdDisplayContainer(adContainer: view, viewController: viewController, companionSlots: [])
+            return IMAAdDisplayContainer(adContainer: view,
+                                         viewController: viewController,
+                                         companionSlots: [])
         }
     }
 }
